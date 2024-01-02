0ef09188c4a310dc0edcc0a5351d855328fa4190
ghdtjgus76 <ghdtjgus76@naver.com>
2024-01-03 00:31:13 +0900

    feat: react native, typescript, storybook 설치

---
+++ .storybook/main.js
@@ -0,0 +1,12 @@
+module.exports = {
+  stories: ["../components/**/*.stories.?(ts|tsx|js|jsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-react-native-web",
+  ],
+  framework: "@storybook/react",
+  core: {
+    builder: 'webpack5',
+  },
+};
