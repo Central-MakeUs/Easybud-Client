b3337fb97553f1194a48ed97c010cf73fb84cfc1
ghdtjgus76 <ghdtjgus76@naver.com>
2024-01-03 01:07:31 +0900

    feat: recoil 세팅

--- App.tsx
+++ App.tsx
@@ -1,2 +1,15 @@
-import StorybookUIRoot from "./.ondevice/Storybook";
-export { StorybookUIRoot as default };
+import {View} from 'react-native';
+import {RecoilRoot} from 'recoil';
+
+const App = () => {
+  return (
+    <RecoilRoot>
+      <View />
+    </RecoilRoot>
+  );
+};
+
+export default App;
+
+// import StorybookUIRoot from './.ondevice/Storybook';
+// export {StorybookUIRoot as default};
