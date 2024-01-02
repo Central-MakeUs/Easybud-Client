9483f0e3d9e313120d9f5a2606424065f8c30925
ghdtjgus76 <ghdtjgus76@naver.com>
2024-01-03 01:00:03 +0900

    feat: 절대 경로 세팅

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,35 @@
 module.exports = {
-  presets: ["module:metro-react-native-babel-preset"],
+  presets: ['module:metro-react-native-babel-preset'],
   plugins: [
-    ["babel-plugin-react-docgen-typescript", { exclude: "node_modules" }],
+    ['babel-plugin-react-docgen-typescript', {exclude: 'node_modules'}],
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@apis': './src/apis',
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@constants': './src/constants',
+          '@libs': './src/libs',
+          '@models': './src/models',
+          '@navigators': './src/navigators',
+          '@screens': './src/screens',
+          '@styles': './src/styles',
+          '@utils': './src/utils',
+        },
+      },
+    ],
   ],
 };
