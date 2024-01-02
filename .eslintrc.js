366a5edc036d381653dd527359a27c4b8460956a
ghdtjgus76 <ghdtjgus76@naver.com>
2024-01-03 02:28:37 +0900

    feat: eslint 관련 세팅

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,21 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    '@react-native-community',
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['android', 'ios', '.ondevice'],
+  settings: {
+    'import/resolver': {
+      'babel-module': {},
+    },
+  },
   rules: {
     'prettier/prettier': [
       'error',
