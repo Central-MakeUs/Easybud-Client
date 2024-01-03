3c480e5848fdc06f8223a1a57713b19a68796cf8
nimod7890 <nimod1234@g.skku.edu>
2024-01-03 22:27:04 +0900

    design: light/dark palette 추가

--- src/types/theme.ts
+++ src/types/theme.ts
@@ -1,11 +1,22 @@
-import {palette, theme} from 'styles';
+import {lightPalette, theme} from 'styles';
 
 export type TypeOfTheme = {
   palette: TypeOfPalette;
 };
 
-export type TypeOfPalette = typeof palette;
+export type TypeOfPalette = Readonly<{
+  primary: string;
+  secondary: string;
+  gray1: string;
+  gray2: string;
+  gray3: string;
+  gray4: string;
+  gray5: string;
+  gray6: string;
+  white: string;
+  black: string;
+}>;
 
-export type KeyOfPalette = keyof typeof palette;
+export type KeyOfPalette = keyof typeof lightPalette;
 
 export type KeyofTheme = keyof typeof theme;
