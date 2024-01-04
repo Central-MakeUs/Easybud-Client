a0321cbac4db072834ef85a4d0d119bcee03c217
ghdtjgus76 <ghdtjgus76@naver.com>
2024-01-04 21:14:26 +0900

    feat: AddTransaction 관련 타입 추가 및 TabRouteProps 타입 수정

--- src/navigators/types/index.ts
+++ src/navigators/types/index.ts
@@ -1,11 +1,18 @@
+import {RouteProp} from '@react-navigation/native';
+
 export type StackParamList = {Home: undefined; TabNavigator: undefined};
 
 export type TabParamList = {
   Ledger: undefined;
   Transaction: undefined;
+  AddTransaction: undefined;
   Account: undefined;
   Setting: undefined;
 };
 
 export type StackScreenName = keyof StackParamList;
 export type TabScreenName = keyof TabParamList;
+
+export type TabRouteProps = {
+  route: RouteProp<TabParamList, TabScreenName>;
+};
