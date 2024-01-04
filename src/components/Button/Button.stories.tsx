297c1086c73501f531184cbfdc1796de55848701
ghdtjgus76 <ghdtjgus76@naver.com>
2024-01-05 02:04:04 +0900

    refactor: 버튼 스토리 네이밍 변경

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -13,32 +13,32 @@ type Story = ComponentStory<typeof Button>;
 
 const Template: Story = args => <Button {...args} />;
 
-export const Button1 = Template.bind({});
-Button1.args = {
+export const FullWidthPrimary = Template.bind({});
+FullWidthPrimary.args = {
   variant: 'primary',
   fullWidth: true,
   isActive: true,
   children: '내용',
 };
 
-export const Button2 = Template.bind({});
-Button2.args = {
+export const DisabledPrimary = Template.bind({});
+DisabledPrimary.args = {
   variant: 'primary',
   fullWidth: true,
   isActive: false,
   children: '내용',
 };
 
-export const Button3 = Template.bind({});
-Button3.args = {
+export const Primary = Template.bind({});
+Primary.args = {
   variant: 'primary',
   fullWidth: false,
   isActive: true,
   children: '내용',
 };
 
-export const Button4 = Template.bind({});
-Button4.args = {
+export const Secondary = Template.bind({});
+Secondary.args = {
   variant: 'secondary',
   fullWidth: false,
   isActive: true,
