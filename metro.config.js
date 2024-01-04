c36c8c29ac996a6d32c523e4f30b3f30a9084998
ghdtjgus76 <ghdtjgus76@naver.com>
2024-01-04 16:29:20 +0900

    feat: 스토리북 svg 관련 세팅 추가

--- metro.config.js
+++ metro.config.js
@@ -1,21 +1,22 @@
-const {getDefaultConfig} = require('metro-config');
+const { getDefaultConfig } = require('metro-config');
 
 module.exports = (async () => {
   const {
-    resolver: {sourceExts, assetExts},
+    resolver: { sourceExts, assetExts },
   } = await getDefaultConfig();
   return {
     transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
       getTransformOptions: async () => ({
         transform: {
           experimentalImportSupport: false,
-          inlineRequires: true,
+          inlineRequires: false,
         },
       }),
-      babelTransformerPath: require.resolve('react-native-svg-transformer'),
     },
     resolver: {
-      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      resolverMainFields: ['sbmodern', 'react-native', 'browser', 'main'],
+      assetExts: assetExts.filter((ext) => ext !== 'svg'),
       sourceExts: [...sourceExts, 'svg'],
     },
   };
