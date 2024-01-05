002623e2f59a6436680133cfe42fc01d1520230d
nimod7890 <nimod1234@g.skku.edu>
2024-01-05 22:10:30 +0900

    feat: storybook 설정 추가

--- .ondevice/storybook.requires.js
+++ .ondevice/storybook.requires.js
@@ -6,37 +6,37 @@ import {
   addParameters,
   addArgsEnhancer,
   clearDecorators,
-} from '@storybook/react-native';
-
-import '@storybook/addon-ondevice-notes/register';
-import '@storybook/addon-ondevice-controls/register';
-import '@storybook/addon-ondevice-backgrounds/register';
-import '@storybook/addon-ondevice-actions/register';
-
-import {argsEnhancers} from '@storybook/addon-actions/dist/modern/preset/addArgs';
-
-import {decorators, parameters} from './preview';
+} from "@storybook/react-native";
 
 global.STORIES = [
   {
-    titlePrefix: '',
-    directory: './src',
-    files: '**/*.stories.?(ts|tsx|js|jsx)',
+    titlePrefix: "",
+    directory: "./src",
+    files: "**/*.stories.?(ts|tsx|js|jsx)",
     importPathMatcher:
-      '^\\.[\\\\/](?:src(?:[\\\\/](?!\\.)(?:(?:(?!(?:^|[\\\\/])\\.).)*?)[\\\\/]|[\\\\/]|$)(?!\\.)(?=.)[^\\\\/]*?\\.stories\\.(?:ts|tsx|js|jsx)?)$',
+      "^\\.[\\\\/](?:src(?:\\/(?!\\.)(?:(?:(?!(?:^|\\/)\\.).)*?)\\/|\\/|$)(?!\\.)(?=.)[^/]*?\\.stories\\.(?:ts|tsx|js|jsx)?)$",
   },
 ];
+
+import "@storybook/addon-ondevice-notes/register";
+import "@storybook/addon-ondevice-controls/register";
+import "@storybook/addon-ondevice-backgrounds/register";
+import "@storybook/addon-ondevice-actions/register";
+
+import { argsEnhancers } from "@storybook/addon-actions/dist/modern/preset/addArgs";
+
+import { decorators, parameters } from "./preview";
 
 if (decorators) {
   if (__DEV__) {
     // stops the warning from showing on every HMR
-    require('react-native').LogBox.ignoreLogs([
-      '`clearDecorators` is deprecated and will be removed in Storybook 7.0',
+    require("react-native").LogBox.ignoreLogs([
+      "`clearDecorators` is deprecated and will be removed in Storybook 7.0",
     ]);
   }
   // workaround for global decorators getting infinitely applied on HMR, see https://github.com/storybookjs/react-native/issues/185
   clearDecorators();
-  decorators.forEach(decorator => addDecorator(decorator));
+  decorators.forEach((decorator) => addDecorator(decorator));
 }
 
 if (parameters) {
@@ -44,12 +44,13 @@ if (parameters) {
 }
 
 try {
-  argsEnhancers.forEach(enhancer => addArgsEnhancer(enhancer));
+  argsEnhancers.forEach((enhancer) => addArgsEnhancer(enhancer));
 } catch {}
 
 const getStories = () => {
   return {
-    './src/components/Button/Button.stories': require('../src/components/Button/Button.stories'),
+    "./src/components/Button/Button.stories.tsx": require("../src/components/Button/Button.stories.tsx"),
+    "./src/components/TextField/TextField.stories.tsx": require("../src/components/TextField/TextField.stories.tsx"),
   };
 };
 
---
+++ src/components/TextField/TextField.stories.tsx
@@ -0,0 +1,23 @@
+import {ComponentStory} from '@storybook/react';
+import TextField from 'components/TextField';
+
+const meta = {
+  title: 'component/TextField',
+  component: TextField,
+};
+
+export default meta;
+
+type Story = ComponentStory<typeof TextField>;
+
+const Template: Story = args => <TextField {...args} />;
+
+export const DefaultStatus = Template.bind({});
+DefaultStatus.args = {
+  value: '',
+};
+
+export const FilledStatus = Template.bind({});
+FilledStatus.args = {
+  value: '입력 완료',
+};
