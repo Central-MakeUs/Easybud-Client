c1b49db5dad7783dfe385753144a30da8c052fd7
bomin <nimod1234@g.skku.edu>
2024-01-06 00:04:02 +0900

    [TAS-38] screen container 생성 (#8)
    * feat: screen container 생성
    
    * design: color 변경

---
+++ src/components/@common/ScreenContainer.tsx
@@ -0,0 +1,68 @@
+import React, {ReactNode} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Platform,
+  ActivityIndicator,
+  View,
+  ScrollViewProps,
+} from 'react-native';
+import {theme} from 'styles';
+
+type ScreenContainerProps = {
+  children: ReactNode;
+  loading?: boolean;
+} & ScrollViewProps;
+
+/** Use ScrollViewProps */
+export default function ScreenContainer({
+  children,
+  loading,
+  ...props
+}: ScreenContainerProps) {
+  return (
+    <SafeAreaView {...props} style={styles.safeArea}>
+      <StatusBar />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={styles.keyboardAvoidingView}>
+        <ScrollView
+          {...props}
+          contentContainerStyle={[
+            styles.scrollViewContent,
+            props.contentContainerStyle,
+          ]}>
+          {loading ? (
+            <View style={styles.loadingContainer}>
+              <ActivityIndicator size="large" color={theme.palette.primary} />
+            </View>
+          ) : (
+            children
+          )}
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: theme.palette.gray1,
+  },
+  keyboardAvoidingView: {
+    flex: 1,
+  },
+  scrollViewContent: {
+    flexGrow: 1,
+    padding: 18,
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
