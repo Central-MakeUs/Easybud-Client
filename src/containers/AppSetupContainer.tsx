71accdaff9a8533421022155fe239bbd90ecea6e
nimod7890 <nimod1234@g.skku.edu>
2024-01-06 23:03:09 +0900

    refactor: safe area provider 이동

--- src/containers/AppSetupContainer.tsx
+++ src/containers/AppSetupContainer.tsx
@@ -1,13 +1,16 @@
 import React, {ReactNode} from 'react';
 import RecoilContainer from 'containers/RecoilContainer';
 import ReactQueryContainer from 'containers/ReactQueryContainer';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 type AppSetupContainerProps = {children: ReactNode};
 
 export default function AppSetupContainer({children}: AppSetupContainerProps) {
   return (
     <RecoilContainer>
-      <ReactQueryContainer>{children}</ReactQueryContainer>
+      <ReactQueryContainer>
+        <SafeAreaProvider>{children}</SafeAreaProvider>
+      </ReactQueryContainer>
     </RecoilContainer>
   );
 }
