027d6dc295f19d4b92428e1db9616347ac77af39
nimod7890 <nimod1234@g.skku.edu>
2024-01-07 15:01:01 +0900

    feat: transaction page로 이동

--- src/navigators/components/AddTransactionButton.tsx
+++ src/navigators/components/AddTransactionButton.tsx
@@ -1,11 +1,14 @@
+import {useNavigation} from '@react-navigation/native';
 import Icon from 'components/@common/Icon';
 import {TabNavigatorIcon} from 'navigators/constants/icon';
 import React from 'react';
 import {TouchableOpacity} from 'react-native';
 
 export default function AddTransactionButton() {
+  const navigation = useNavigation();
+
   return (
-    <TouchableOpacity>
+    <TouchableOpacity onPress={() => navigation.navigate('AddTransaction')}>
       <Icon name={TabNavigatorIcon.AddTransaction} fill={'primary'} size={44} />
     </TouchableOpacity>
   );
