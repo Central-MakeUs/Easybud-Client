ccea932ca4102b28e95156108cbaa07202d237bb
nimod7890 <nimod1234@g.skku.edu>
2024-01-08 02:08:47 +0900

    refactor: funnel

--- src/screens/OnBoardingFunnelScreen/index.tsx
+++ src/screens/OnBoardingFunnelScreen/index.tsx
@@ -1,25 +1,27 @@
-import Funnel from 'components/@common/funnel/Funnel';
-import Step from 'components/@common/funnel/Step';
 import React, {useState} from 'react';
 import DescriptionStepScreen from 'screens/OnBoardingFunnelScreen/DescriptionStepScreen';
 import LoginStepScreen from 'screens/OnBoardingFunnelScreen/LoginStepScreen';
 import UserInfoStepScreen from 'screens/OnBoardingFunnelScreen/UserInfoStepScreen';
-import {NonEmptyArray, OnBoardingStep, StepInfo} from 'types/funnel';
+import type {NonEmptyArray, Steps} from 'types/funnel';
+import Funnel from 'components/@common/funnel/Funnel';
+import Step from 'components/@common/funnel/Step';
+
+type OnBoardingStep = 'Step1' | 'Step2' | 'Step3';
 
 const steps: NonEmptyArray<OnBoardingStep> = ['Step1', 'Step2', 'Step3'];
 
-const stepInfoList: StepInfo<'Step'>[] = [
-  {name: 'Step1', component: <DescriptionStepScreen />},
-  {name: 'Step2', component: <LoginStepScreen />},
-  {name: 'Step3', component: <UserInfoStepScreen />},
-];
+const stepInfoList: Steps<typeof steps> = {
+  Step1: <DescriptionStepScreen />,
+  Step2: <LoginStepScreen />,
+  Step3: <UserInfoStepScreen />,
+};
 
 export default function OnBoardingFunnelScreen() {
   const [currentStep] = useState<OnBoardingStep>('Step1');
 
   return (
     <Funnel steps={steps} step={currentStep}>
-      {stepInfoList.map(({name, component}) => (
+      {Object.entries(stepInfoList).map(([name, component]) => (
         <Step key={name} name={name}>
           {component}
         </Step>
