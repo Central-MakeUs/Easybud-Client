4f3b916d070835d4a1c72be61a145a272fb9748f
ghdtjgus76 <ghdtjgus76@naver.com>
2024-01-08 16:30:27 +0900

    chore: TextField multiline 속성 추가 및 props 수정

--- src/components/@common/TextField/index.tsx
+++ src/components/@common/TextField/index.tsx
@@ -1,8 +1,10 @@
 import Icon from 'components/@common/Icon';
 import React, {useState} from 'react';
 import {
+  NativeSyntheticEvent,
   StyleSheet,
   TextInput,
+  TextInputContentSizeChangeEventData,
   TextInputProps,
   TouchableOpacity,
   View,
@@ -10,26 +12,34 @@ import {
 import {theme} from 'styles';
 
 /**
- * @param value 텍스트 필드에 표시될 값
- * @param onChangeText 값이 변경될 때 호출. 변경된 텍스트가 매개변수로 전달
- * @param ...props - react native TextInputProps
+ * @param defaultValue 텍스트 필드에 맨 처음에 표시될 기본값
+ * @param placeholder 텍스트 필드 placeholder
  */
-type TextFieldProps = {
-  value: string;
-  onChangeText: (text: string) => void;
-} & Omit<TextInputProps, 'value' | 'onChangeText'>;
 
-export default function TextField({
-  value,
-  onChangeText,
-  ...props
-}: TextFieldProps) {
+type TextFieldProps = {defaultValue: string} & Pick<
+  TextInputProps,
+  'placeholder'
+>;
+
+export default function TextField({defaultValue, ...props}: TextFieldProps) {
+  const [text, setText] = useState(defaultValue ?? '');
+  const [height, setHeight] = useState(0);
   const [isFocused, setIsFocused] = useState<boolean>(false);
 
   const handleFocus = () => setIsFocused(true);
   const handleBlur = () => setIsFocused(false);
 
-  const clearInput = () => onChangeText('');
+  const clearInput = () => {
+    setText('');
+    setHeight(0);
+  };
+
+  const handleInputHeight = (
+    e: NativeSyntheticEvent<TextInputContentSizeChangeEventData>,
+  ) => {
+    e.nativeEvent.contentSize.height &&
+      setHeight(e.nativeEvent.contentSize.height);
+  };
 
   return (
     <View
@@ -37,19 +47,22 @@ export default function TextField({
         styles.container,
         {
           borderBottomColor:
-            theme.palette[isFocused || value ? 'primary' : 'gray3'],
+            theme.palette[isFocused || text ? 'primary' : 'gray3'],
         },
       ]}>
       <TextInput
-        value={value}
-        onChangeText={onChangeText}
+        value={text}
+        onChangeText={setText}
         placeholder={props.placeholder ?? '내용을 입력해주세요.'}
         onFocus={handleFocus}
         onBlur={handleBlur}
-        style={styles.text}
+        multiline={true}
+        underlineColorAndroid="transparent"
+        onContentSizeChange={handleInputHeight}
+        style={[styles.text, {height}]}
       />
-      {value !== '' && (
-        <TouchableOpacity onPress={clearInput} style={styles.clearButton}>
+      {text !== '' && (
+        <TouchableOpacity onPress={clearInput}>
           <Icon name="XCircle" color={theme.palette.gray3} />
         </TouchableOpacity>
       )}
@@ -71,7 +84,7 @@ const styles = StyleSheet.create({
   text: {
     ...theme.typography.Title1Semibold1,
     placeholderTextColor: theme.palette.gray3,
-    maxWidth: '93%',
+    width: '100%',
+    height: '100%',
   },
-  clearButton: {},
 });
