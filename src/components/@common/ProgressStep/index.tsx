c058fd9d621fe7239a75331618ac778fdb791c6e
ghdtjgus76 <ghdtjgus76@naver.com>
2024-01-17 06:07:41 +0900

    feat: ProgressStep 컴포넌트 구현

---
+++ src/components/@common/ProgressStep/index.tsx
@@ -0,0 +1,47 @@
+import {StyleSheet, View} from 'react-native';
+import {theme} from 'styles';
+
+type ProgressStepProps = {
+  stepCount: number;
+  currentStep: number;
+};
+
+export default function ProgressStep({
+  stepCount,
+  currentStep,
+}: ProgressStepProps) {
+  return (
+    <View style={progressStepStyles.container}>
+      {[...Array(stepCount)].map((_, index) => (
+        <View
+          key={index}
+          style={[
+            progressStepStyles.bar,
+            {
+              backgroundColor:
+                index + 1 === currentStep
+                  ? theme.palette.primary
+                  : theme.palette.gray2,
+            },
+          ]}
+        />
+      ))}
+    </View>
+  );
+}
+
+const progressStepStyles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: 8,
+    backgroundColor: theme.palette.gray2,
+    borderRadius: 8,
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  bar: {
+    flex: 1,
+    height: 10,
+    borderRadius: 8,
+  },
+});
