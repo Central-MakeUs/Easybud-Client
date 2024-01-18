d3ede97113e39c730494f647394b8d82dc5357d6
ghdtjgus76 <ghdtjgus76@naver.com>
2024-01-18 19:55:37 +0900

    feat: BottomSheet 컴포넌트 추가

---
+++ src/components/@common/BottomSheet/index.tsx
@@ -0,0 +1,56 @@
+import {Dispatch, ReactNode, SetStateAction, useEffect, useRef} from 'react';
+import {StyleSheet} from 'react-native';
+import RBSheet from 'react-native-raw-bottom-sheet';
+import {theme} from 'styles';
+
+type BottomSheetProps = {
+  isBottomSheetOpen: boolean;
+  setIsBottomSheetOpen: Dispatch<SetStateAction<boolean>>;
+  height?: number;
+  children: ReactNode;
+};
+
+export default function BottomSheet({
+  isBottomSheetOpen,
+  setIsBottomSheetOpen,
+  height,
+  children,
+}: BottomSheetProps) {
+  const bottomSheetRef = useRef<RBSheet>(null);
+
+  useEffect(() => {
+    isBottomSheetOpen
+      ? bottomSheetRef.current!.open()
+      : bottomSheetRef.current!.close();
+  }, [isBottomSheetOpen]);
+
+  const handleOpenBottomSheet = () => {
+    setIsBottomSheetOpen(true);
+  };
+
+  const handleCloseBottomSheet = () => {
+    setIsBottomSheetOpen(false);
+  };
+
+  return (
+    <RBSheet
+      ref={bottomSheetRef}
+      onOpen={handleOpenBottomSheet}
+      onClose={handleCloseBottomSheet}
+      keyboardAvoidingViewEnabled={true}
+      height={height ?? 200}
+      customStyles={bottomSheetStyles}>
+      {children}
+    </RBSheet>
+  );
+}
+
+const bottomSheetStyles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    padding: 15,
+    borderTopStartRadius: 20,
+    borderTopEndRadius: 20,
+    backgroundColor: theme.palette.gray2,
+  },
+});
