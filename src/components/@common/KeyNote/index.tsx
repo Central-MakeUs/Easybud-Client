3bbccad1d9004ba1a2bb2402ddf147ce4e6eb2f9
ghdtjgus76 <ghdtjgus76@naver.com>
2024-01-19 05:44:50 +0900

    refactor: KeyNote 컴포넌트 CommonSelectItem 컴포넌트로 공통 로직 추상화

--- src/components/@common/KeyNote/index.tsx
+++ src/components/@common/KeyNote/index.tsx
@@ -1,72 +1,32 @@
 import {useState} from 'react';
-import {StyleSheet, TouchableOpacity, View} from 'react-native';
 import {sliceString} from 'utils/sliceString';
-import {theme} from 'styles';
-import Icon from 'components/@common/Icon';
-import Typography from 'components/@common/Typography';
 import KeyNoteBottomSheet from 'components/@common/KeyNote/KeyNoteBottomSheet';
-import {KeyOfPalette} from 'styles/types';
+import CommonSelectItem from 'components/@common/CommonSelectItem';
 
 export default function KeyNote() {
   const [keyNoteText, setKeyNoteText] = useState('');
   const [keyNoteInputText, setKeyNoteInputText] = useState('');
   const [isBottomSheetOpen, setIsBottomSheetOpen] = useState(false);
 
-  const handlePressRightCol = () => setIsBottomSheetOpen(true);
+  const handlePressSelectItem = () => setIsBottomSheetOpen(true);
 
-  const {color, text}: {color: KeyOfPalette; text: string} = {
-    color: keyNoteText === '' ? 'gray3' : 'gray5',
-    text: keyNoteText === '' ? '입력하세요' : sliceString(keyNoteText, 16),
-  };
+  const text = keyNoteText === '' ? '입력하세요' : sliceString(keyNoteText, 30);
 
   return (
-    <View style={keyNoteStyles.container}>
-      <Typography type={'Body1Semibold'} color={'gray4'}>
-        적요
-      </Typography>
-      <TouchableOpacity
-        onPress={handlePressRightCol}
-        style={keyNoteStyles.rightCol}>
-        <Typography
-          type={'Body1Semibold'}
-          color={color}
-          style={keyNoteStyles.text}>
-          {text}
-        </Typography>
-        <Icon name={'ArrowRightSmall'} fill={'gray4'} />
-      </TouchableOpacity>
-      <KeyNoteBottomSheet
-        isBottomSheetOpen={isBottomSheetOpen}
-        setIsBottomSheetOpen={setIsBottomSheetOpen}
-        keyNoteInputText={keyNoteInputText}
-        setKeyNoteText={setKeyNoteText}
-        setKeyNoteInputText={setKeyNoteInputText}
-      />
-    </View>
+    <CommonSelectItem
+      label={'적요'}
+      variant={'gray'}
+      handlePressSelectItem={handlePressSelectItem}
+      value={text}
+      bottomSheet={
+        <KeyNoteBottomSheet
+          isBottomSheetOpen={isBottomSheetOpen}
+          setIsBottomSheetOpen={setIsBottomSheetOpen}
+          keyNoteInputText={keyNoteInputText}
+          setKeyNoteText={setKeyNoteText}
+          setKeyNoteInputText={setKeyNoteInputText}
+        />
+      }
+    />
   );
 }
-
-const keyNoteStyles = StyleSheet.create({
-  container: {
-    width: '100%',
-    height: 68,
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    borderBottomWidth: 1,
-    borderBottomColor: theme.palette.gray2,
-  },
-  rightCol: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    gap: 15,
-    flex: 1,
-    marginLeft: 50,
-  },
-  text: {
-    flex: 1,
-  },
-});
