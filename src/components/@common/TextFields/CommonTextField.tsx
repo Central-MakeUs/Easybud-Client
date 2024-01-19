5eabdbe240bff1835bcb8d7858e21a896df6babf
ghdtjgus76 <ghdtjgus76@naver.com>
2024-01-19 19:06:27 +0900

    refactor: TextField 컴포넌트 리팩토링 및 타입 주석 추가

--- src/components/@common/TextFields/CommonTextField.tsx
+++ src/components/@common/TextFields/CommonTextField.tsx
@@ -11,17 +11,25 @@ import {
 import {theme} from 'styles';
 import Icon from 'components/@common/Icon';
 import Typography from 'components/@common/Typography';
+import {Dispatch, SetStateAction} from 'react';
 
+/**
+ * @param isAmountField AmountTextField인지 여부
+ * @param isFocused text input의 focus 여부
+ * @param label label 텍스트
+ * @param height text input의 높이
+ * @param setHeight text input의 높이를 변경하는 함수
+ * @param setIsFocused text input의 focus 여부를 변경하는 함수
+ * @param handleClearInput text input의 X 아이콘을 눌렀을 때 동작하는 함수
+ * @param handleKeyPress text input에서 특정 키를 눌렀을 때 동작하는 함수
+ */
 type CommonTextFieldProps = TextInputProps & {
   isAmountField?: boolean;
   isFocused: boolean;
   label?: string;
   height: number;
-  handleBlur: () => void;
-  handleFocus: () => void;
-  handleInputHeight: (
-    e: NativeSyntheticEvent<TextInputContentSizeChangeEventData>,
-  ) => void;
+  setHeight: Dispatch<SetStateAction<number>>;
+  setIsFocused: Dispatch<SetStateAction<boolean>>;
   handleClearInput: () => void;
   handleKeyPress?: (
     e: NativeSyntheticEvent<TextInputKeyPressEventData>,
@@ -33,15 +41,25 @@ export default function CommonTextField({
   isFocused,
   value,
   height,
+  setHeight,
+  setIsFocused,
   onChangeText,
-  handleBlur,
-  handleFocus,
-  handleInputHeight,
   handleClearInput,
   handleKeyPress,
   placeholder,
   label,
 }: CommonTextFieldProps) {
+  const handleInputHeight = (
+    e: NativeSyntheticEvent<TextInputContentSizeChangeEventData>,
+  ) => {
+    e.nativeEvent.contentSize.height &&
+      setHeight(e.nativeEvent.contentSize.height);
+  };
+
+  const handleFocus = () => setIsFocused(true);
+
+  const handleBlur = () => setIsFocused(false);
+
   return (
     <View
       style={[
