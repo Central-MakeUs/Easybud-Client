1e34d7919da04fee5f6c8ef16b2ffd62beb2e6d9
ghdtjgus76 <ghdtjgus76@naver.com>
2024-01-19 18:15:36 +0900

    comment: 퍼널, 스텝 컴포넌트 타입 주석 추가

--- src/components/@common/Funnel/Funnel.tsx
+++ src/components/@common/Funnel/Funnel.tsx
@@ -1,6 +1,11 @@
 import {Children, ReactElement, isValidElement} from 'react';
 import {NonEmptyArray, StepProps} from 'types/funnel';
 
+/**
+ * @param steps 전체 스텝 문자열 리스트
+ * @param step 현재 스텝을 나타내는 문자열
+ * @param children 전체 스텝 컴포넌트 배열
+ */
 type FunnelProps<Steps extends NonEmptyArray<string>> = {
   steps: Steps;
   step: Steps[number];
