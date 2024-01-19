12b8ab3b379ba5aa89c385c46bc6e73d7ba82d99
nimod7890 <nimod1234@g.skku.edu>
2024-01-19 21:44:46 +0900

    fix: non-serializable values props로 전달 가능하도록 지정

--- src/containers/ErrorBoundaryContainer.tsx
+++ src/containers/ErrorBoundaryContainer.tsx
@@ -1,6 +1,12 @@
 import {ReactElement} from 'react';
+import {LogBox} from 'react-native';
 import ErrorBoundary from 'react-native-error-boundary';
 import FallbackErrorScreen from 'screens/FallbackErrorScreen';
+
+// https://reactnavigation.org/docs/troubleshooting/
+LogBox.ignoreLogs([
+  'Non-serializable values were found in the navigation state',
+]);
 
 type ErrorBoundaryContainerProps = {children: ReactElement};
 
