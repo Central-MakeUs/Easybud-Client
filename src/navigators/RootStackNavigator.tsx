6722eff089022f7c55fb3d3d7fad5e1d2094a063
ghdtjgus76 <ghdtjgus76@naver.com>
2024-01-21 18:14:46 +0900

    fix: Authenticated 스크린 삭제

--- src/navigators/RootStackNavigator.tsx
+++ src/navigators/RootStackNavigator.tsx
@@ -7,8 +7,10 @@ import {theme} from 'styles';
 import {RootStackNavigationProp, RootStackParamList} from 'navigators/types';
 import useInitialData from 'hooks/useInitialData';
 import OnBoardingFunnelScreen from 'screens/OnBoardingFunnelScreen';
-import AuthenticatedScreens from 'screens/AuthenticatedScreens';
 import Icon from 'components/@common/Icon';
+import CreateTransactionStackNavigator from 'navigators/CreateTransactionStackNavigator';
+import TabNavigator from 'navigators/TabNavigator';
+import SettingScreen from 'screens/SettingScreen';
 
 export const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -26,7 +28,29 @@ export default function RootStackNavigator() {
       initialRouteName={initialRouteName}
       screenOptions={screenOptions}>
       {isAuthenticated ? (
-        <AuthenticatedScreens />
+        <>
+          <Stack.Screen name={'Tab'} component={TabNavigator} />
+          <Stack.Screen
+            name={'CreateTransactionStack'}
+            component={CreateTransactionStackNavigator}
+            options={{
+              headerShown: true,
+              headerBackTitleVisible: true,
+              headerTitle: '거래 추가',
+              // note: navigation.push()로 인해 새 계정 추가 시 아래 설정으로 stack이 쌓이기에 통일성을 위해 변경하지 않음
+              // presentation: 'containedModal',
+            }}
+          />
+          <Stack.Screen
+            name={'Setting'}
+            component={SettingScreen}
+            options={{
+              headerShown: true,
+              headerBackTitleVisible: true,
+              headerTitle: '설정',
+            }}
+          />
+        </>
       ) : (
         <Stack.Screen name={'OnBoarding'} component={OnBoardingFunnelScreen} />
       )}
