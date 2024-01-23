759f4561e8d07671c066d2d7599cdcd5d03cf5b9
ghdtjgus76 <ghdtjgus76@naver.com>
2024-01-24 02:13:33 +0900

    refactor: 합성 컴포넌트 패턴으로 리팩토링

--- src/components/@common/TextFields/AmountTextField/index.tsx
+++ src/components/@common/TextFields/AmountTextField/index.tsx
@@ -1,16 +1,6 @@
-import React, {useState} from 'react';
-import {
-  NativeSyntheticEvent,
-  TextInputKeyPressEventData,
-  TextInputProps,
-} from 'react-native';
-import {
-  formatNumberToLocaleString,
-  formatValue,
-  parseNumberFromString,
-} from 'utils/formatAmountValue';
-import CommonTextField from 'components/@common/TextFields/CommonTextField';
-import DescriptionText from 'components/@common/TextFields/DescriptionText';
+import React from 'react';
+import {TextInputProps} from 'react-native';
+import {CommonTextFieldBase} from 'components/@common/TextFields/CommonTextField';
 
 /**
  * @param defaultCurrentBalance 현재 대차를 나타내는 텍스트
@@ -21,50 +11,16 @@ export default function AmountTextField({
   defaultValue,
   defaultCurrentBalance,
 }: TextFieldProps) {
-  const [value, setValue] = useState(formatValue(defaultValue) ?? '');
-  const [isFocused, setIsFocused] = useState<boolean>(false);
-  const [height, setHeight] = useState(0);
-
-  const handleClearInput = () => {
-    setValue('0원');
-    setHeight(56);
-  };
-
-  const onChangeText = (text: string) => setValue(formatValue(text));
-
-  const handleKeyPress = (
-    e: NativeSyntheticEvent<TextInputKeyPressEventData>,
-  ) => {
-    if (e.nativeEvent.key === 'Backspace') {
-      if (value !== '0원') {
-        const parsedValue = parseNumberFromString(value).slice(0, -1);
-
-        setValue(`${formatNumberToLocaleString(parsedValue)}원`);
-      }
-
-      e.preventDefault();
-    }
-  };
-
   return (
-    <>
-      <CommonTextField
-        isAmountField={true}
-        value={value}
-        label="금액"
-        isFocused={isFocused}
-        height={height}
-        setHeight={setHeight}
-        setIsFocused={setIsFocused}
-        onChangeText={onChangeText}
-        handleClearInput={handleClearInput}
-        handleKeyPress={handleKeyPress}
-      />
-      <DescriptionText
-        value={value}
-        setValue={setValue}
+    <CommonTextFieldBase defaultValue={defaultValue}>
+      <CommonTextFieldBase.Container>
+        <CommonTextFieldBase.Label label="금액" />
+        <CommonTextFieldBase.CustomTextInput isAmountField={true} />
+        <CommonTextFieldBase.ClearIcon />
+      </CommonTextFieldBase.Container>
+      <CommonTextFieldBase.TextFieldHelperText
         defaultCurrentBalance={defaultCurrentBalance}
       />
-    </>
+    </CommonTextFieldBase>
   );
 }
--- src/components/@common/TextFields/CommonTextField.tsx
+++ src/components/@common/TextFields/CommonTextField.tsx
@@ -1,9 +1,16 @@
 import {
+  Dispatch,
+  ReactElement,
+  SetStateAction,
+  createContext,
+  useContext,
+  useState,
+} from 'react';
+import {
   View,
   TextInput,
   TouchableOpacity,
   StyleSheet,
-  TextInputProps,
   NativeSyntheticEvent,
   TextInputKeyPressEventData,
   TextInputContentSizeChangeEventData,
@@ -11,7 +18,12 @@ import {
 import {theme} from 'styles';
 import Icon from 'components/@common/Icon';
 import Typography from 'components/@common/Typography';
-import {Dispatch, SetStateAction} from 'react';
+import {
+  formatNumberToLocaleString,
+  formatValue,
+  parseNumberFromString,
+} from 'utils/formatAmountValue';
+import DescriptionText from 'components/@common/TextFields/DescriptionText';
 
 /**
  * @param isAmountField AmountTextField인지 여부
@@ -23,42 +35,9 @@ import {Dispatch, SetStateAction} from 'react';
  * @param handleClearInput text input의 X 아이콘을 눌렀을 때 동작하는 함수
  * @param handleKeyPress text input에서 특정 키를 눌렀을 때 동작하는 함수
  */
-type CommonTextFieldProps = TextInputProps & {
-  isAmountField?: boolean;
-  isFocused: boolean;
-  label?: string;
-  height: number;
-  setHeight: Dispatch<SetStateAction<number>>;
-  setIsFocused: Dispatch<SetStateAction<boolean>>;
-  handleClearInput: () => void;
-  handleKeyPress?: (
-    e: NativeSyntheticEvent<TextInputKeyPressEventData>,
-  ) => void;
-};
 
-export default function CommonTextField({
-  isAmountField = false,
-  isFocused,
-  value,
-  height,
-  setHeight,
-  setIsFocused,
-  onChangeText,
-  handleClearInput,
-  handleKeyPress,
-  placeholder,
-  label,
-}: CommonTextFieldProps) {
-  const handleInputHeight = (
-    e: NativeSyntheticEvent<TextInputContentSizeChangeEventData>,
-  ) => {
-    e.nativeEvent.contentSize.height &&
-      setHeight(e.nativeEvent.contentSize.height);
-  };
-
-  const handleFocus = () => setIsFocused(true);
-
-  const handleBlur = () => setIsFocused(false);
+export function Container({children}: {children: ReactElement[]}) {
+  const commonTextFieldContext = useContext(CommonTextFieldContext);
 
   return (
     <View
@@ -66,39 +45,175 @@ export default function CommonTextField({
         commonTextFieldStyles.textFieldContainer,
         {
           borderBottomColor:
-            theme.palette[isFocused || value ? 'primary' : 'gray3'],
+            theme.palette[
+              commonTextFieldContext?.isFocused || commonTextFieldContext?.value
+                ? 'primary'
+                : 'gray3'
+            ],
         },
       ]}>
-      {value && (
-        <Typography
-          type={'Body2Regular'}
-          color={'gray3'}
-          style={commonTextFieldStyles.label}>
-          {label}
-        </Typography>
-      )}
-      <TextInput
-        value={value}
-        onChangeText={onChangeText}
-        placeholder={placeholder}
-        placeholderTextColor={theme.palette.gray3}
-        onFocus={handleFocus}
-        onBlur={handleBlur}
-        onKeyPress={e => handleKeyPress?.(e)}
-        multiline={true}
-        underlineColorAndroid="transparent"
-        onContentSizeChange={handleInputHeight}
-        keyboardType={isAmountField ? 'phone-pad' : 'default'}
-        style={[commonTextFieldStyles.textInput, {height}]}
-      />
-      {value !== '' && (
-        <TouchableOpacity onPress={handleClearInput}>
-          <Icon name="XCircle" color={theme.palette.gray3} />
-        </TouchableOpacity>
-      )}
+      {children}
     </View>
   );
 }
+
+export function Label({label}: {label: string}) {
+  const commonTextFieldContext = useContext(CommonTextFieldContext);
+
+  return (
+    commonTextFieldContext?.value && (
+      <Typography
+        type={'Body2Regular'}
+        color={'gray3'}
+        style={commonTextFieldStyles.label}>
+        {label}
+      </Typography>
+    )
+  );
+}
+
+export function CustomTextInput({
+  isAmountField,
+  placeholder,
+}: {
+  isAmountField: boolean;
+  placeholder?: string;
+}) {
+  const commonTextFieldContext = useContext(CommonTextFieldContext);
+
+  const handleInputHeight = (
+    e: NativeSyntheticEvent<TextInputContentSizeChangeEventData>,
+  ) => {
+    e.nativeEvent.contentSize.height &&
+      commonTextFieldContext?.setHeight?.(e.nativeEvent.contentSize.height);
+  };
+
+  const handleFocus = () => commonTextFieldContext?.setIsFocused?.(true);
+
+  const handleBlur = () => commonTextFieldContext?.setIsFocused?.(false);
+
+  return (
+    <TextInput
+      value={commonTextFieldContext?.value}
+      onChangeText={commonTextFieldContext?.onChangeText}
+      placeholder={placeholder}
+      placeholderTextColor={theme.palette.gray3}
+      onFocus={handleFocus}
+      onBlur={handleBlur}
+      onKeyPress={(e: NativeSyntheticEvent<TextInputKeyPressEventData>) =>
+        commonTextFieldContext?.handleKeyPress?.(e)
+      }
+      multiline={true}
+      underlineColorAndroid="transparent"
+      onContentSizeChange={handleInputHeight}
+      keyboardType={isAmountField ? 'phone-pad' : 'default'}
+      style={[
+        commonTextFieldStyles.textInput,
+        {height: commonTextFieldContext?.height},
+      ]}
+    />
+  );
+}
+
+export function ClearIcon() {
+  const commonTextFieldContext = useContext(CommonTextFieldContext);
+
+  return (
+    commonTextFieldContext?.value !== '' && (
+      <TouchableOpacity onPress={commonTextFieldContext?.handleClearInput}>
+        <Icon name="XCircle" color={theme.palette.gray3} />
+      </TouchableOpacity>
+    )
+  );
+}
+
+export function TextFieldHelperText({
+  defaultCurrentBalance,
+}: {
+  defaultCurrentBalance?: string;
+}) {
+  const commonTextFieldContext = useContext(CommonTextFieldContext);
+
+  return (
+    <DescriptionText
+      value={commonTextFieldContext?.value ?? ''}
+      setValue={commonTextFieldContext?.setValue!}
+      defaultCurrentBalance={defaultCurrentBalance}
+    />
+  );
+}
+
+const CommonTextFieldContext = createContext<CommonTextFieldProps | undefined>(
+  undefined,
+);
+
+type CommonTextFieldProps = {
+  children?: ReactElement[];
+  defaultValue?: string;
+  value?: string;
+  setValue?: Dispatch<SetStateAction<string>>;
+  isFocused?: boolean;
+  setIsFocused?: Dispatch<SetStateAction<boolean>>;
+  height?: number;
+  setHeight?: Dispatch<SetStateAction<number>>;
+  handleClearInput?: () => void;
+  onChangeText?: (text: string) => void;
+  handleKeyPress?: (
+    e: NativeSyntheticEvent<TextInputKeyPressEventData>,
+  ) => void;
+};
+
+function CommonTextField({children, defaultValue}: CommonTextFieldProps) {
+  const [value, setValue] = useState(formatValue(defaultValue) ?? '');
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+  const [height, setHeight] = useState(0);
+
+  const handleClearInput = () => {
+    setValue('0원');
+    setHeight(56);
+  };
+
+  const onChangeText = (text: string) => setValue(formatValue(text));
+
+  const handleKeyPress = (
+    e: NativeSyntheticEvent<TextInputKeyPressEventData>,
+  ) => {
+    if (e.nativeEvent.key === 'Backspace') {
+      if (value !== '0원') {
+        const parsedValue = parseNumberFromString(value).slice(0, -1);
+
+        setValue(`${formatNumberToLocaleString(parsedValue)}원`);
+      }
+
+      e.preventDefault();
+    }
+  };
+
+  return (
+    <CommonTextFieldContext.Provider
+      value={{
+        value,
+        setValue,
+        isFocused,
+        setIsFocused,
+        height,
+        setHeight,
+        handleClearInput,
+        onChangeText,
+        handleKeyPress,
+      }}>
+      {children}
+    </CommonTextFieldContext.Provider>
+  );
+}
+
+export const CommonTextFieldBase = Object.assign(CommonTextField, {
+  Container,
+  Label,
+  CustomTextInput,
+  ClearIcon,
+  TextFieldHelperText,
+});
 
 const commonTextFieldStyles = StyleSheet.create({
   textFieldContainer: {
