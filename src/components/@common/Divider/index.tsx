c9590f377f79c52a2c589f197d4ea4236892ef66
ghdtjgus76 <ghdtjgus76@naver.com>
2024-01-25 19:51:15 +0900

    chore: Divider props 추가

--- src/components/@common/Divider/index.tsx
+++ src/components/@common/Divider/index.tsx
@@ -1,18 +1,36 @@
 import {View, ViewStyle} from 'react-native';
 import {theme} from 'styles';
+import {KeyOfPalette} from 'styles/types';
 
+/**
+ * @param color divider 배경색
+ * @param height divider 높이
+ * @param style 추가적인 스타일 속성
+ */
 type DividerProps = {
+  color?: KeyOfPalette;
+  height?: number;
   style?: ViewStyle | ViewStyle[];
 };
 
-export default function Divider({style}: DividerProps) {
-  return <View style={[style, dividerStyles.divider]} />;
+export default function Divider({
+  color = 'gray3',
+  height = 7,
+  style,
+}: DividerProps) {
+  return (
+    <View
+      style={[
+        style,
+        dividerStyles.divider,
+        {height, backgroundColor: theme.palette[color]},
+      ]}
+    />
+  );
 }
 
 const dividerStyles = {
   divider: {
     width: '100%',
-    height: 7,
-    backgroundColor: theme.palette.gray3,
   },
 };
