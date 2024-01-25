48ddf786007fbb0dc3a5737eb76ba7c486f0dfa2
ghdtjgus76 <ghdtjgus76@naver.com>
2024-01-25 19:52:03 +0900

    design: 장부 탭 css 속성 적용

--- src/screens/Tab/LedgerScreen.tsx
+++ src/screens/Tab/LedgerScreen.tsx
@@ -1,10 +1,22 @@
 import ScreenContainer from 'components/@common/ScreenContainer';
 import RecentTransaction from 'components/screen/LedgerScreen/RecentTransaction';
+import Divider from 'components/@common/Divider';
 
 export default function LedgerScreen() {
   return (
-    <ScreenContainer>
+    <ScreenContainer
+      contentContainerStyle={ledgerScreenStyles.contentContainer}>
       <RecentTransaction />
+      <Divider style={ledgerScreenStyles.divider} />
     </ScreenContainer>
   );
 }
+
+const ledgerScreenStyles = {
+  contentContainer: {
+    paddingHorizontal: 0,
+  },
+  divider: {
+    marginVertical: 15,
+  },
+};
