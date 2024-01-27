8be4fcea99615089b7206ad497fcbb6f05bb2db0
ghdtjgus76 <ghdtjgus76@naver.com>
2024-01-27 19:29:37 +0900

    feat: 툴팁 컴포넌트 추가

--- src/components/screen/LedgerScreen/FInancialStatusOverview/index.tsx
+++ src/components/screen/LedgerScreen/FInancialStatusOverview/index.tsx
@@ -2,29 +2,47 @@ import React, {useState} from 'react';
 import {StyleSheet, View} from 'react-native';
 import {FinancialDataCardBase} from 'components/screen/LedgerScreen/FinancialDataCard';
 import FinancialStatusBottomElement from 'components/screen/LedgerScreen/FinancialStatusOverview/FinancialStatusBottomElement';
+import Tooltip from 'components/@common/Tooltip';
 
 export default function FinancialStatusOverview() {
   const [hasInitialNetWorth, _] = useState(false);
+  const [showTooltipText, setShowTooltipText] = useState(false);
 
   return (
-    <FinancialDataCardBase.Container>
-      <FinancialDataCardBase.TopElementContainer>
-        <View style={financialStatusOverviewStyles.topElementFirstColContainer}>
-          <FinancialDataCardBase.Label label={'재무상태'} />
-          {!hasInitialNetWorth && <FinancialDataCardBase.TooltipIcon />}
-        </View>
-        <FinancialDataCardBase.DetailButton />
-      </FinancialDataCardBase.TopElementContainer>
-      <FinancialDataCardBase.BottomElement
-        children={
-          <FinancialStatusBottomElement
-            networthAmount={170450000}
-            assetAmount={20045000}
-            debtAmount={3000000}
-          />
-        }
-      />
-    </FinancialDataCardBase.Container>
+    <View>
+      <FinancialDataCardBase.Container>
+        <FinancialDataCardBase.TopElementContainer>
+          <View
+            style={financialStatusOverviewStyles.topElementFirstColContainer}>
+            <FinancialDataCardBase.Label label={'재무상태'} />
+            {!hasInitialNetWorth && (
+              <FinancialDataCardBase.TooltipIcon
+                onPress={() => setShowTooltipText(prevState => !prevState)}
+              />
+            )}
+          </View>
+          <FinancialDataCardBase.DetailButton />
+        </FinancialDataCardBase.TopElementContainer>
+        <FinancialDataCardBase.BottomElement
+          children={
+            <FinancialStatusBottomElement
+              networthAmount={170450000}
+              assetAmount={20045000}
+              debtAmount={3000000}
+            />
+          }
+        />
+      </FinancialDataCardBase.Container>
+      {showTooltipText && (
+        <Tooltip
+          text={'기초 순자산을 설정하세요!'}
+          position={{
+            top: 29,
+            left: 23,
+          }}
+        />
+      )}
+    </View>
   );
 }
 
