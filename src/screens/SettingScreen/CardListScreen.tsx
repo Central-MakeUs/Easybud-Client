3d0a442aefe2085d450983602234e33cddaa4521
ghdtjgus76 <ghdtjgus76@naver.com>
2024-01-31 00:32:05 +0900

    feat: 카드 설정 페이지 카드 리스트 페이지 추가

--- src/screens/SettingScreen/CardListScreen.tsx
+++ src/screens/SettingScreen/CardListScreen.tsx
@@ -1,22 +1,23 @@
-import {View} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import ScreenContainer from 'components/@common/ScreenContainer';
 import Button from 'components/@common/Buttons/Button';
+import CardItem from 'components/screens/SettingScreen/CardItem';
+import {StyleSheet} from 'react-native';
 
-// const mockCardListData = [
-//   {
-//     name: '신한카드',
-//     usagePeriod: '1일 ~ 말일',
-//     paymentDate: '1일',
-//     keyNote: '쿠팡',
-//   },
-//   {
-//     name: '신한카드',
-//     usagePeriod: '1일 ~ 말일',
-//     paymentDate: '1일',
-//     keyNote: '쿠팡',
-//   },
-// ];
+const mockCardListData = [
+  {
+    name: '신한카드',
+    usagePeriod: '1일 ~ 말일',
+    paymentDate: '1일',
+    keyNote: '쿠팡',
+  },
+  {
+    name: '신한카드',
+    usagePeriod: '1일 ~ 말일',
+    paymentDate: '1일',
+    keyNote: '쿠팡',
+  },
+];
 
 export default function CardListScreen() {
   const navigation = useNavigation();
@@ -27,8 +28,22 @@ export default function CardListScreen() {
     <ScreenContainer
       fixedBottomComponent={
         <Button onPress={handlePressAddCardButton}>카드 추가</Button>
-      }>
-      <View />
+      }
+      contentContainerStyle={cardListScreenStyles.contentContainer}>
+      {mockCardListData.map(cardData => (
+        <CardItem
+          cardName={cardData.name}
+          usagePeriod={cardData.usagePeriod}
+          paymentDate={cardData.paymentDate}
+          keyNote={cardData.keyNote}
+        />
+      ))}
     </ScreenContainer>
   );
 }
+
+const cardListScreenStyles = StyleSheet.create({
+  contentContainer: {
+    gap: 20,
+  },
+});
