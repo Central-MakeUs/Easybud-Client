52a4998c0e1d263fbbd1bb02dd0f26c86694c693
ghdtjgus76 <ghdtjgus76@naver.com>
2024-01-31 05:28:04 +0900

    refactor: 쿼리 키 상수로 분리

--- src/hooks/queries/useGetRecentTransactionQuery.ts
+++ src/hooks/queries/useGetRecentTransactionQuery.ts
@@ -1,9 +1,10 @@
 import {useQuery} from '@tanstack/react-query';
 import {ledgerApi} from 'apis/ledgerApi';
+import {ledgerQueryKeys} from 'constants/queryKeys/ledger';
 
 export const useGetRecentTransactionQuery = () => {
   const {data: recentTransactionList = []} = useQuery({
-    queryKey: ['recentTransactionList'],
+    queryKey: [ledgerQueryKeys.recentTransactionList],
     queryFn: ledgerApi.getRecentTransactions,
   });
 
