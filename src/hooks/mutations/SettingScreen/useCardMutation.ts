87c0eb0201485523d4d448f224ffe11a04e50723
ghdtjgus76 <ghdtjgus76@naver.com>
2024-02-01 04:35:39 +0900

    fix: 카드 삭제, 카드 추가 후 invalidateQueries 기능 동작하도록 수정

--- src/hooks/mutations/SettingScreen/useCardMutation.ts
+++ src/hooks/mutations/SettingScreen/useCardMutation.ts
@@ -1,10 +1,21 @@
-import {useMutation} from '@tanstack/react-query';
+import {useMutation, useQueryClient} from '@tanstack/react-query';
 import {settingApi} from 'apis/settingApi';
+import {settingQueryKeys} from 'constants/queryKeys/setting';
 
 export const useCardMutation = () => {
-  const removeCardMutation = useMutation({mutationFn: settingApi.removeCard});
+  const queryClient = useQueryClient();
 
-  const addCardMutation = useMutation({mutationFn: settingApi.addCard});
+  const removeCardMutation = useMutation({
+    mutationFn: settingApi.removeCard,
+    onSuccess: () =>
+      queryClient.invalidateQueries({queryKey: [settingQueryKeys.cardList]}),
+  });
+
+  const addCardMutation = useMutation({
+    mutationFn: settingApi.addCard,
+    onSuccess: () =>
+      queryClient.invalidateQueries({queryKey: [settingQueryKeys.cardList]}),
+  });
 
   return {removeCardMutation, addCardMutation};
 };
