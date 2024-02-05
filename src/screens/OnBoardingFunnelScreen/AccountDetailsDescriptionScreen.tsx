394e4495dd38de2eaa035ec7924d2a4c7820c514
ghdtjgus76 <ghdtjgus76@naver.com>
2024-02-05 19:59:50 +0900

    feat: 온보딩 튜토리얼 4페이지 구현

--- src/screens/OnBoardingFunnelScreen/AccountDetailsDescriptionScreen.tsx
+++ src/screens/OnBoardingFunnelScreen/AccountDetailsDescriptionScreen.tsx
@@ -1,10 +1,33 @@
-import {View} from 'react-native';
+import {StyleSheet} from 'react-native';
+import {theme} from 'styles';
 import ScreenContainer from 'components/@common/ScreenContainer';
+import Typography from 'components/@common/Typography';
+import Button from 'components/@common/Buttons/Button';
 
 export default function AccountDetailsDescriptionScreen() {
   return (
-    <ScreenContainer>
-      <View />
+    <ScreenContainer
+      fixedBottomComponent={<Button variant="secondary">다음</Button>}
+      contentContainerStyle={accountDetailsDescriptionScreenStyles.container}>
+      <Typography type={'Title3SemiBold'} color={'white'}>
+        차・대변 데이터를 바탕으로
+      </Typography>
+      <Typography
+        type={'Title3SemiBold'}
+        color={'white'}
+        style={accountDetailsDescriptionScreenStyles.text}>
+        입력한 거래를 검토해요
+      </Typography>
     </ScreenContainer>
   );
 }
+
+const accountDetailsDescriptionScreenStyles = StyleSheet.create({
+  container: {
+    backgroundColor: theme.palette.primary,
+    paddingTop: 30,
+  },
+  text: {
+    lineHeight: 39,
+  },
+});
