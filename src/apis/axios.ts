f17f3e7c77ddc7ead1fcfa566bb80646352cd788
ghdtjgus76 <ghdtjgus76@naver.com>
2024-02-06 00:42:32 +0900

    feat: 토큰 만료 시 재발급 로직 추가

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -1,4 +1,6 @@
-import axios from 'axios';
+import axios, {AxiosError, AxiosResponse} from 'axios';
+import {authApi} from 'apis/authApi';
+import localStorage from 'libs/async-storage';
 
 export const baseURL = 'https://easybud.store/api';
 
@@ -10,3 +12,58 @@ export const axiosApi = axios.create({
     'Content-Type': 'application/json',
   },
 });
+
+axiosApi.interceptors.request.use(
+  config => {
+    const accessToken = localStorage.get('accessToken');
+
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  },
+);
+
+const onFulfilled = (res: AxiosResponse) => {
+  return res;
+};
+
+const onRejected = async (error: AxiosError) => {
+  const originalConfig = error.config;
+
+  try {
+    const value = await localStorage.get('localStorage');
+    const refreshToken = value as string;
+
+    if (
+      (originalConfig && error.response?.status === 401) ||
+      error.response?.status === 500
+    ) {
+      const result = await authApi.patchAccessToken({
+        refreshToken,
+      });
+
+      axiosApi.request({
+        ...originalConfig,
+        headers: {
+          Authorization: `Bearer ${result.accessToken}`,
+        },
+      });
+
+      localStorage.set('accessToken', result.accessToken);
+      localStorage.set('refreshToken', result.refreshToken);
+
+      throw new Error('Access token refreshed. Retry the original request.');
+    }
+  } catch (e) {
+    throw new Error(
+      'Error occurred during token refresh. Retry the original request.',
+    );
+  }
+};
+
+axiosApi.interceptors.response.use(onFulfilled, onRejected);
