fa1a018fe51bdeb3459d5e8ad60433cd54d1eb05
ghdtjgus76 <ghdtjgus76@naver.com>
2024-02-08 03:30:47 +0900

    design: 온보딩 튜토리얼 1 페이지 자잘한 디자인 수정

--- src/screens/OnBoardingFunnelScreen/LedgerDescriptionScreen.tsx
+++ src/screens/OnBoardingFunnelScreen/LedgerDescriptionScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Animated, StyleSheet} from 'react-native';
+import {StyleSheet} from 'react-native';
 import {theme} from 'styles';
 import {SetStepActionType} from 'types/screens/FunnelScreen';
 import ScreenContainer from 'components/@common/ScreenContainer';
@@ -18,30 +18,24 @@ export default function LedgerDescriptionScreen({
           다음
         </Button>
       }
-      contentContainerStyle={styles.container}>
-      <Animated.View>
-        <Typography type={'Title3SemiBold'} color={'white'}>
-          나만의 장부를 만들어보세요
-        </Typography>
-        <Typography type={'Title3SemiBold'} color={'white'} style={styles.text}>
-          거래 내역과 재무 데이터를 확인할 수 있어요
-        </Typography>
-      </Animated.View>
+      contentContainerStyle={ledgerDescriptionScreenStyles.container}>
+      <Typography type={'Title3SemiBold'} color={'white'}>
+        나만의 장부를 만들어보세요
+      </Typography>
+      <Typography
+        type={'Title3SemiBold'}
+        color={'white'}
+        style={ledgerDescriptionScreenStyles.text}>
+        거래 내역과 재무 데이터를 확인할 수 있어요
+      </Typography>
     </ScreenContainer>
   );
 }
 
-const styles = StyleSheet.create({
+const ledgerDescriptionScreenStyles = StyleSheet.create({
   container: {
     backgroundColor: theme.palette.primary,
     paddingTop: 30,
-    justifyContent: 'space-between',
-    paddingHorizontal: 20,
-  },
-  content: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   text: {
     lineHeight: 39,
