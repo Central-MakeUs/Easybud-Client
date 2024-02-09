ccea95a66ee72af522dac01882c62f9d74c2c7ef
ghdtjgus76 <ghdtjgus76@naver.com>
2024-02-09 18:41:26 +0900

    feat: 온보딩 튜토리얼 1 페이지 이미지 추가 및 자잘한 디자인 변경

--- src/screens/OnBoardingFunnelScreen/LedgerDescriptionScreen.tsx
+++ src/screens/OnBoardingFunnelScreen/LedgerDescriptionScreen.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Image, View} from 'react-native';
 import {theme} from 'styles';
+import ledgerDescription from 'assets/images/ledger-description.png';
 import {SetStepActionType} from 'types/screens/FunnelScreen';
 import ScreenContainer from 'components/@common/ScreenContainer';
 import Typography from 'components/@common/Typography';
@@ -14,30 +15,51 @@ export default function LedgerDescriptionScreen({
   return (
     <ScreenContainer
       fixedBottomComponent={
-        <Button variant="secondary" onPress={onNext}>
+        <Button variant="primary" onPress={onNext}>
           다음
         </Button>
       }
       contentContainerStyle={ledgerDescriptionScreenStyles.container}>
-      <Typography type={'Title3SemiBold'} color={'white'}>
-        나만의 장부를 만들어보세요
-      </Typography>
-      <Typography
-        type={'Title3SemiBold'}
-        color={'white'}
-        style={ledgerDescriptionScreenStyles.text}>
-        거래 내역과 재무 데이터를 확인할 수 있어요
-      </Typography>
+      <View style={ledgerDescriptionScreenStyles.textContainer}>
+        <Typography type={'Title3SemiBold'} color={'black'}>
+          나만의 장부를 만들어보세요
+        </Typography>
+        <Typography
+          type={'Title3SemiBold'}
+          color={'black'}
+          style={ledgerDescriptionScreenStyles.text}>
+          거래 내역과 재무 데이터를 확인할 수 있어요
+        </Typography>
+      </View>
+      <View style={ledgerDescriptionScreenStyles.imageContainer}>
+        <Image
+          source={ledgerDescription}
+          style={ledgerDescriptionScreenStyles.image}
+        />
+      </View>
     </ScreenContainer>
   );
 }
 
 const ledgerDescriptionScreenStyles = StyleSheet.create({
   container: {
-    backgroundColor: theme.palette.primary,
+    backgroundColor: theme.palette.gray2,
     paddingTop: 30,
+    paddingHorizontal: 0,
+    gap: 10,
+  },
+  textContainer: {
+    paddingHorizontal: 20,
   },
   text: {
     lineHeight: 39,
+  },
+  imageContainer: {
+    flex: 1,
+    marginBottom: 80,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
   },
 });
