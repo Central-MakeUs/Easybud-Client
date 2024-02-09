a9077a634f2c1faaa4bd19e5ee942e1f3c90fe2d
홍서현 <81316050+ghdtjgus76@users.noreply.github.com>
2024-02-09 15:48:31 +0900

    [TAS-145] 안드로이드 스플래시 화면 적용 (#51)
    * fix: axios interceptor 로직 수정
    
    * feat: 안드로이드 앱 아이콘 적용
    
    * feat: 안드로이드 스플래시 화면 적용

--- android/app/src/main/java/com/easybud/MainActivity.java
+++ android/app/src/main/java/com/easybud/MainActivity.java
@@ -1,9 +1,11 @@
 package com.easybud;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -32,4 +34,10 @@ public class MainActivity extends ReactActivity {
         DefaultNewArchitectureEntryPoint.getConcurrentReactEnabled() // concurrentRootEnabled
         );
   }
+
+  @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);
+        super.onCreate(savedInstanceState);
+    }
 }
