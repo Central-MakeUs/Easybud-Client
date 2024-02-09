4d1c927f4576a5304dd4585a8073721934ef0670
홍서현 <81316050+ghdtjgus76@users.noreply.github.com>
2024-02-10 04:01:07 +0900

    [TAS-157] 빈 스크린이 보이는 문제 해결 (#56)

--- src/navigators/components/NullScreen.tsx
+++ src/navigators/components/NullScreen.tsx
@@ -1,3 +1,15 @@
-const NullScreen = () => null;
+import {useFocusEffect, useNavigation} from '@react-navigation/native';
+
+const NullScreen = () => {
+  const navigation = useNavigation();
+
+  useFocusEffect(() => {
+    navigation.navigate('CreateTransactionStack', {
+      screen: 'BasicTransactionInfo',
+    });
+  });
+
+  return null;
+};
 
 export default NullScreen;
