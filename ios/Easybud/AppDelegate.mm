0b44d2e707fb826a61351609bea2b8e067fbf25d
nimod7890 <nimod1234@g.skku.edu>
2024-02-13 00:45:30 +0900

    fix: splash screen 안 뜨는 오류 수정

--- ios/Easybud/AppDelegate.mm
+++ ios/Easybud/AppDelegate.mm
@@ -1,6 +1,7 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import "RNSplashScreen.h"
 
 @implementation AppDelegate
 
@@ -10,8 +11,10 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+  
+  [super application:application didFinishLaunchingWithOptions:launchOptions];
+  [RNSplashScreen show]; //splash screen 
+  return YES;
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
