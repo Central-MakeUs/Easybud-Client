3ae58257199e0a036ff938b2c75b0d79f880df92
nimod7890 <nimod1234@g.skku.edu>
2024-02-14 01:13:44 +0900

    feat: kakao login

--- ios/Easybud/AppDelegate.mm
+++ ios/Easybud/AppDelegate.mm
@@ -1,9 +1,18 @@
+
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
 #import "RNSplashScreen.h"
+#import <RNKakaoLogins.h>
 
 @implementation AppDelegate
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+ if([RNKakaoLogins isKakaoTalkLoginUrl:url]) {
+    return [RNKakaoLogins handleOpenUrl: url];
+ }
+
+ return NO;
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
